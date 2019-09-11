4f01bd56b20c91c83c8dc83218cc1abe0febd340
Sergioramasano <razumenko.serg@gmail.com>
2019-09-11 22:35:35 +0300

    ref

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$( function() {
+    $( "#progress-1" ).progressbar({
+        value: 75
+    });
+} );
+$( function() {
+    $( "#progress-2" ).progressbar({
+        value: 90
+    });
+} );
+$( function() {
+    $( "#progress-3" ).progressbar({
+        value: 65
+    });
+
+} );
+
+
+
+
+
+
